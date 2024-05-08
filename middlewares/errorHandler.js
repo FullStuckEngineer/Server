92d9f8019c362bc4163ebddc3e7aec07ddae9267
haniffIndraa11 <111898576+haniffIndraa11@users.noreply.github.com>
2024-05-08 22:07:31 +0700

    feature address dan error handler (#1)
    * feature address dan error handler
    
    * delete console.log

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,10 @@
 const errorHandler = (err, req, res, next) => {
     console.log(err)
-    res.status(500).json({ message: "Internal Server Error"})
+    if (err.name === "ErrorNotFound") {
+        res.status(404).json({ message: "Error Not Found" })
+    } else {
+        res.status(500).json({ message: "Internal Server Error" })
+    }
 }
 
 module.exports = errorHandler
