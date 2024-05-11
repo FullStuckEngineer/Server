13e290d3a895248e1b6fdd08ecec78c12aacf613
Husnia Munzayana <13521077@std.stei.itb.ac.id>
2024-05-11 22:12:41 +0700

    fix: role authorization

--- routes/cms/productRoute.js
+++ routes/cms/productRoute.js
@@ -3,11 +3,11 @@ const productController = require("../../controllers/cms/productController")
 const { authorization } = require("../../middlewares/auth")
 const upload = require('../../middlewares/multer');
 
-router.get("/", authorization(["Admin"]), productController.findAll)
-router.get("/:slug", authorization(["Admin"]), productController.findOne)
-router.post("/", authorization(["Admin"]), productController.create)
-router.post("/uploads", authorization(["Admin"]), upload.single('image'), productController.uploadImage)
-router.put("/:id", authorization(["Admin"]), productController.update)
-router.delete("/:id", authorization(["Admin"]), productController.destroy)
+router.get("/", authorization(["admin"]), productController.findAll)
+router.get("/:slug", authorization(["admin"]), productController.findOne)
+router.post("/", authorization(["admin"]), productController.create)
+router.post("/uploads", authorization(["admin"]), upload.single('image'), productController.uploadImage)
+router.put("/:id", authorization(["admin"]), productController.update)
+router.delete("/:id", authorization(["admin"]), productController.destroy)
 
 module.exports = router
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const productController = require("../controllers/productController")
 const { authorization } = require("../middlewares/auth")
 
-router.get("/", authorization(["User"]), productController.findAll)
-router.get("/:slug", authorization(["User"]), productController.findOne)
+router.get("/", authorization(["user"]), productController.findAll)
+router.get("/:slug", authorization(["user"]), productController.findOne)
 
 module.exports = router
