e0e0f8713717b9d68ec7685882d06fbdf1e1f0ad
Husnia Munzayana <13521077@std.stei.itb.ac.id>
2024-05-11 19:10:57 +0700

    feat: product services

--- services/productService.js
+++ services/productService.js
@@ -17,8 +17,33 @@ const findOne = async (params) => {
     return product;
 } 
 
+const generateSlug = (name) => {
+    const slugifiedName = name.toLowerCase().replace(/\s+/g, '-');
+    const randomNumber = Math.floor(1000 + Math.random() * 9000);
+    const slug = `${slugifiedName}-${randomNumber}`;
+    return slug;
+}
+
 const create = async (params) => {
-    const { name, description, price, weight, category_id, stock, sku, slug, keywords } = params;
+    const { name, description, price, weight, category_id, stock, sku, keywords } = params;
+
+    if (stock < 0 || price < 0 || weight < 0) {
+        throw { name: "Stock, price, and weight cannot be negative" };
+    }
+    
+    const slug = generateSlug(name);
+
+    const category = await prisma.category.findFirst({
+        where: {
+            id: parseInt(category_id),
+            status: 'Active'
+        }
+    });
+
+    if (!category) {
+        throw { name: "Category Not Found or Inactive" };
+    }
+
     const product = await prisma.product.create({
         data: {
             name,
@@ -58,15 +83,36 @@ const uploadImage = async (params) => {
 } 
 
 const update = async (params) => {
-    const { id, name, photo, description, price, weight, category_id, stock, sku, slug, keywords, shopping_items, checkout_products } = params;
+    const { id, name, description, price, weight, category_id, stock, sku, keywords, shopping_items, checkout_products } = params;
+
+    if ((stock && stock < 0) || (price && price < 0) || (wight && weight < 0)) {
+        throw { name: "Stock, price, and weight cannot be negative" };
+    }
+
+    if (category_id) {
+        const category = await prisma.category.findFirst({
+            where: {
+                id: parseInt(category_id),
+                status: 'Active'
+            }
+        });
+
+        if (!category) {
+            throw { name: "Category Not Found or Inactive" };
+        }
+    }
+
+    if (name) {
+        slug = createSlug(name);
+    }
+
     const dataToUpdate = {
         ...(name && { name }),
-        ...(photo && { photo }),
         ...(description && { description }),
-        ...(price !== undefined && { price }),
-        ...(weight !== undefined && { weight }),
-        ...(category_id !== undefined && { category_id }),
-        ...(stock !== undefined && { stock }),
+        ...(price && { price }),
+        ...(weight && { weight }),
+        ...(category_id && { category_id }),
+        ...(stock && { stock }),
         ...(sku && { sku }),
         ...(slug && { slug }),
         ...(keywords && { keywords }),
@@ -87,15 +133,18 @@ const update = async (params) => {
 }
 
 const destroy = async (params) => {
-    const { id } = params;
-    const product = await prisma.product.delete({
+    const productId = parseInt(params.id);
+    const product = await prisma.product.update({
         where: {
-            id
+            id: productId
+        },
+        data: {
+            status: "Inactive"
         }
     })
     if (!product) throw { name: "Failed to Delete Image" };
 
     return product;
-} 
+}
 
 module.exports = { findAll, findOne, create, update, destroy, uploadImage }
