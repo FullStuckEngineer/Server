abd9dfc84830f19c757ef871453540bacba2fa2d
Husnia Munzayana <88820186+munzayanahusn@users.noreply.github.com>
2024-05-13 01:33:33 +0700

    Feature/category (#6)
    * feat: endpoint API categories CMS
    
    * feat: endpoint cetarogies user
    
    * fix: endpoint delete category
    
    * feat: product endpoint
    
    * fix: del find all params
    
    * fix: del unused authentication
    
    * feat: add service error handling
    
    * feat: user product controller
    
    * feat: admin product controller
    
    * feat: product service error handling
    
    * fix db: add status field for softdelete
    
    * fix: change delete to soft delete
    
    * feat: product services
    
    * fix: category services valudation and product soft delete
    
    * feat: category pagination and separate based auth
    
    * fix: add import product service
    
    * fix: status based auth in findOne
    
    * fix: findone
    
    * fix: destroy category
    
    * fix: role auth

--- controllers/cms/productController.js
+++ controllers/cms/productController.js
@@ -1,15 +1,53 @@
 const productService = require('../../services/productService')
 
-const findAll = async (req, res, next) => {} 
+const findAll = async (req, res, next) => {
+    try{
+        const products = await productService.findAll();
+        res.status(200).json({message: "Products Found", data: products});
+    } catch(err){
+        next(err);
+    }
+} 
 
-const findOne = async (req, res, next) => {} 
+const findOne = async (req, res, next) => {
+    try{
+        const product = await productService.findOne(req.params);
+        res.status(200).json({message: "Product By ID Found", data:product});
+    } catch(err){
+        next(err);
+    }
+} 
 
-const create = async (req, res, next) => {} 
+const create = async (req, res, next) => {
+    try{
+        const product = await productService.create(req.body);
+        res.status(200).json({message: "Product Created", data:product});
+    } catch(err){
+        next(err);
+    }
+} 
 
-const uploadImage = async (req, res, next) => {} 
+const uploadImage = async (req, res, next) => {
+    try {
+        const filePath = req.file.path;
+        const productId = req.body.id;
+        const uploadImage = await productService.uploadImage({productId, filePath});
+
+        res.status(200).json({message: "Image Uploaded", data:uploadImage});
+    } catch(err){
+        next
+    }
+} 
 
 const update = async (req, res, next) => {} 
 
-const destroy = async (req, res, next) => {} 
+const destroy = async (req, res, next) => {
+    try {
+        const product = await productService.destroy(req.params);
+        res.status(200).json({message: "Product Deleted", data:product});
+    } catch(err){
+        next(err);
+    }
+} 
 
 module.exports = { findAll, findOne, create, update, destroy, uploadImage }
