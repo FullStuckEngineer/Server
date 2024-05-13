4f9daa2186f464399b49d87257bd95195cef916d
haniff-tech <ramadhan28123@gmail.com>
2024-05-13 18:40:50 +0700

    store feature fix, errorHandler update, address fix at cms

--- controllers/cms/storeController.js
+++ controllers/cms/storeController.js
@@ -1,13 +1,51 @@
 const storeService = require('../../services/storeService')
 
-const findAll = async (req, res, next) => {} 
+const findAll = async (req, res, next) => {
+    try {
+        const store = await storeService.findAll(req.loggedUser)
+        res.status(200).json(store)
+    } catch (error) {
+        next(error)
+    }
+}
 
-const findOne = async (req, res, next) => {} 
+const findOne = async (req, res, next) => {
+    try {
+        const params = {user_id: req.loggedUser.id, id: req.params.id }
+        const findStore = await storeService.findOne(params)
+        res.status(200).json(findStore)
+    } catch (error) {
+        next(error)
+    }
+}
 
-const create = async (req, res, next) => {} 
+const create = async (req, res, next) => {
+    try {
+        const createStore = await storeService.create(req.body)
+        res.status(201).json(createStore)
+    } catch (error) {
+        next(error) 
+    }
+}
 
-const update = async (req, res, next) => {} 
+const update = async (req, res, next) => {
+    try {
+        const params = { id: req.params.id, body: req.body }
+        const updateStore = await storeService.update(params)
+        res.status(200).json(updateStore)
+    } catch (error) {
+        next (error)
+    }
+ }
 
-const destroy = async (req, res, next) => {} 
+const destroy = async (req, res, next) => {
+    try {
+        const params = { id: req.params.id }
+        await storeService.destroy(params)
+        res.status(200).json({message: "Store Deleted"})
+    } catch (error) {
+        next (error)
+    }
+ }
 
 module.exports = { findAll, findOne, create, update, destroy }
