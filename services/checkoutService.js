718903c7de826487e297d1af8a907028911e31c5
haniff-tech <ramadhan28123@gmail.com>
2024-05-13 15:04:02 +0700

    store feature progress 20%

--- services/checkoutService.js
+++ services/checkoutService.js
@@ -2,7 +2,20 @@ const findAll = async (params) => {}
 
 const findOne = async (params) => {} 
 
-const create = async (params) => {} 
+const create = async (params) => {
+    //relasi user, address, courier
+    //user dapat memilih payment method
+    //user dapat memilih bank untuk pembayaran
+    //user akan mendapatkan payment receipt
+    //user akan mendapa status pembayaran, pengiriman?
+    //user akan mendapat invoice
+    //user akan mendapat shipping method
+    //user akan mendapat midtrans data(diperlihatkan)
+    //user akan mendapatkan data total weight, total cost
+    //user akan mendapatkan shipping note
+    //user akan mendapatkan shipping cost
+    //user akan mendapatkan checkout products
+} 
 
 const update = async (params) => {} 
 
--- services/storeService.js
+++ services/storeService.js
@@ -1,11 +1,46 @@
-const findAll = async (params) => {} 
+const prisma = require("../lib/prisma")
 
-const findOne = async (params) => {} 
+const findAll = async (params) => {
+    try {
+        const getAll = await prisma.store.findMany()
+        return getAll
+    } catch (error) {
+        throw error
+    }
+}
 
-const create = async (params) => {} 
+const findOne = async (params) => {
+    const { id } = params
+    try {
+        const getOne = await prisma.store.findUnique({
+            where: { id: Number(id) }
+        })
+        if (!getOne) {
+            throw ({ name: "ErrorNotFound", message: "Store Not Found" })
+        } else {
+            return getOne
+        }
+    } catch (error) {
+        throw error
+    }
+}
 
-const update = async (params) => {} 
+const create = async (params) => {
+    //validasi admin?
+    try {
+     const { body } = params   
+     
+    } catch (error) {
+        throw error
+    }
+ }
 
-const destroy = async (params) => {} 
+const update = async (params) => {
+     //validasi admin?
+ }
+
+const destroy = async (params) => {
+     //validasi admin?
+ }
 
 module.exports = { findAll, findOne, create, update, destroy }
