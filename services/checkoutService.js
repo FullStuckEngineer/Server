5bf1fe596d0baf7a6389071e4bb719049fbc14bb
haniff-tech <ramadhan28123@gmail.com>
2024-05-14 20:24:29 +0700

    first checkout commit

--- services/checkoutService.js
+++ services/checkoutService.js
@@ -45,70 +45,86 @@ const create = async (params) => {
     // - filter
     // - Checkout Product?
     try {
-        const { user_id } = params
+        const { user_id, body } = params
 
         //get courier_id, total_cost, total_weight, shipping_cost from cart
-        const cart = await prisma.cart.findUnique({
-            where: {
-                user_id: Number(user_id)
-            },
-            select: {
-                courier_id: true,
-                total_cost: true,
-                total_weight: true,
-                shiping_cost: true,
-                shopping_items: {
-                    select: {
-                        product_id: true,
-                        quantity: true
-                    }
-                }
-            },
-        })
+        // const cart = await prisma.cart.findUnique({
+        //     where: {
+        //         user_id: Number(user_id)
+        //     },
+        //     select: {
+        //         courier_id: true,
+        //         total_cost: true,
+        //         total_weight: true,
+        //         shiping_cost: true,
+        //         shopping_items: {
+        //             select: {
+        //                 product_id: true,
+        //                 quantity: true
+        //             }
+        //         }
+        //     },
+        // })
         // set default courier_id to 1
-        if (cart.courier_id === null) {
-            await prisma.cart.update({
-                where: {
-                    user_id: Number(user_id)
-                },
-                data: {
-                    courier_id: 1
-                }
-            })
-        }
+        // if (cart.courier_id === null) {
+        //     await prisma.cart.update({
+        //         where: {
+        //             user_id: Number(user_id)
+        //         },
+        //         data: {
+        //             courier_id: 1
+        //         }
+        //     })
+        // }
 
         // check stock product
-        cart.shopping_items.forEach(async (item) => {
-            try {
-                const product = await prisma.product.findUnique({
-                    where: {
-                        id: item.product_id
-                    }
-                })
-                if (product.stock < item.quantity) {
-                    throw ({ name: "ErrorNotEnoughStock", message: "Not enough stock" })
-                }
+        // cart.shopping_items.forEach(async (item) => {
+        //     try {
+        //         const product = await prisma.product.findUnique({
+        //             where: {
+        //                 id: item.product_id
+        //             }
+        //         })
+        //         if (product.stock < item.quantity) {
+        //             throw ({ name: "ErrorNotEnoughStock", message: "Not enough stock" })
+        //         }
 
-                // update stock
-                await prisma.product.update({
-                    where: {
-                        id: item.product_id
-                    },
-                    data: {
-                        stock: product.stock - item.quantity
-                    }
-                })
-            } catch (error) {
-                throw error
+        //         // update stock
+        //         await prisma.product.update({
+        //             where: {
+        //                 id: item.product_id
+        //             },
+        //             data: {
+        //                 stock: product.stock - item.quantity
+        //             }
+        //         })
+        //     } catch (error) {
+        //         throw error
+        //     }
+        // })
+
+        //check address
+        const address = await prisma.address.findUnique({
+            where: {
+                id: Number(body.address_id)
             }
         })
 
+        if (!address) { throw ({ name: "ErrorNotFound", message: "Address Not Found" }) }
+
+        //check courier
+        const courier = await prisma.courier.findUnique({
+            where: {
+                id: Number(body.courier_id)
+            }
+        })
+
+        if(!courier) { throw ({ name: "ErrorNotFound", message: "Courier Not Found" }) }
+
+       
+
         const createCheckout = await prisma.checkout.create({
-            data: {
-                ...params,
-                courier_id: cart.courier_id,
-                total_cost: cart.total_cost,
-                total_weight: cart.total_weight
+            data: body
                 //address?
                 //payment_method?
                 //bank?
@@ -120,7 +136,6 @@ const create = async (params) => {
                 //shipping_cost?
                 //shipping_method?
                 //checkout_products?
-            }
         });
         console.log(cart)
         return createCheckout
