648a2ead31c378a6904ef928637a3c65e1f388cd
Husnia Munzayana <13521077@std.stei.itb.ac.id>
2024-05-22 01:54:46 +0700

    feat: city filtering server side

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -1,11 +1,16 @@
 const cityService = require("../services/cityService");
 
+const perPage = 10;
+
 const findAll = async (req, res, next) => {
     try {
-
-        const params = req.params;
+        params = {
+            page: req.query.page? parseInt(req.query.page) : 1,
+            perPage: perPage,
+            searchTerm: req.query.searchTerm,
+        }
         const cities = await cityService.findAll(params);
-        res.status(200).json(cities);
+        res.status(200).json({message: "Success Get All Cities", data: cities})
     } catch (error) {
         next(error);
     }
--- services/cityService.js
+++ services/cityService.js
@@ -2,8 +2,34 @@ const prisma = require("../lib/prisma");
 
 const findAll = async (params) => {
     try {
-        const cities = await prisma.city.findMany();
-        return cities;
+        const { page = 1, perPage = 10, searchTerm = ''} = params;
+
+        const offset = (page - 1) * perPage;
+        const limit = perPage;
+
+        let where = {};
+        if (searchTerm) {
+            where.OR = [
+                { name: { contains: searchTerm, mode: 'insensitive' } },
+            ]
+        }
+
+        const totalCount = await prisma.city.count({
+            where
+        });
+
+        const cities = await prisma.city.findMany({
+            where,
+            skip: offset,
+            take: limit
+        });
+
+        if (!cities || cities.length === 0) {
+            throw { name: "ErrorNotFound", message: "Cities Not Found" };
+        }
+
+        const totalPages = Math.ceil(totalCount / perPage);
+        return { cities, totalPages };
     } catch (error) {
         throw ({ name: "ErrorNotFound", message: "Cities Not Found" })
     }
