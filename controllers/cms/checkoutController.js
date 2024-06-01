e154b1490043b66ea7ba0b1b7cc9449a83cf87db
Husnia Munzayana <13521077@std.stei.itb.ac.id>
2024-06-01 10:07:08 +0700

    fix: user id create checkout by cms

--- controllers/cms/checkoutController.js
+++ controllers/cms/checkoutController.js
@@ -37,7 +37,7 @@ const findOne = async (req, res, next) => {
 
 const create = async (req, res, next) => {
     try {
-        const obj = { user_id: req.loggedUser.id, body: req.body }
+        const obj = { user_id: req.body.user_id, body: req.body }
         const createCheckout = await checkoutService.create(obj)
         res.status(200).json({message: "Success Create Checkout", data: createCheckout})
     } catch (error) {
